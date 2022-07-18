c4042e43893ea7bdab01aad99272327ad10c1229
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-07-18 15:00:09 +0900

    Create pattern_basic.rb

---
+++ myruby/chap04/4-1/pattern_basic.rb
@@ -0,0 +1,10 @@
+list = ['Ruby', 'Python', 'PHP']
+
+case list
+    in ['Java', 'C#', 'Visual BASIC']
+        puts 'コンパイル方式'
+    in ['Ruby', 'Python', 'PHP']
+        puts 'インタプリンター方式'
+    in ['Kotlin', 'TypeScript']
+        puts 'トランスコンパイル方式'
+end
