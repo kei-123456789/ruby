17eade1f404618c36769751a000bcae1072f74b2
kei-123456789 <104549980+kei-123456789@users.noreply.github.com>
2022-07-22 12:02:07 +0900

    Create rescue_else.rb

---
+++ myruby/chap04/4-5/rescue_else.rb
@@ -0,0 +1,11 @@
+loop do
+    begin
+        print '数字を入力してください。'
+        num = Float(gets)
+        puts "平方根は...#{Math.sqrt(num)}"
+    rescue Math::DomainError => ex
+        puts "エラー発生： #{ex.message}"
+    else
+        break
+    end
+end
